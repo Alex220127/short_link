4115a40db41c36fe5e8db6fdd169aed1633d72cd
AlexBonetti <alex.bonetti@amo.delivery>
2026-05-18 22:40:14 -0300

    feat: add use case to create user

---
+++ src/app/useCases/users/CreateUserUseCase.js
@@ -0,0 +1,39 @@
+import EnumMessage from '../../../domain/enum/EnumMessage.js'
+import BusinessException from '../../../domain/exceptions/BusinessException.js'
+
+const { APP_URL } = process.env
+
+export default class CreateUserUseCase {
+  constructor ({ hashService, tokenService, userRepository, tokenRepository, permissionService }) {
+    this.hashService = hashService
+    this.tokenService = tokenService
+    this.userRepository = userRepository
+    this.tokenRepository = tokenRepository
+    this.permissionService = permissionService
+  }
+
+  execute = async ({ body }) => {
+    body.password = this.hashService.createHash({ content: body.password })
+    const userData = await this.userRepository.create({ entity: body })
+
+    if (!userData) {
+      throw new BusinessException({ message: EnumMessage.ERROR_CREATE_USER })
+    }
+
+    const tokenData = {
+      iss: APP_URL,
+      iat: new Date().getTime(),
+      scope: userData.profile,
+      user_id: userData._id,
+      permissions: this.permissionService.getPermissions({ profile: userData.profile })
+    }
+    const authToken = this.tokenService.signToken({ token: tokenData })
+    await this.tokenRepository.create({ entity: tokenData })
+
+    const data = {
+      access_token: authToken
+    }
+
+    return { data }
+  }
+}
