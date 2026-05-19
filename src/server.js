2a1e9e8919716d0f1f1971d6bf3bd161894f7e58
AlexBonetti <alex.bonetti@amo.delivery>
2026-05-18 22:43:56 -0300

    feat: start of app and database connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 import 'dotenv/config'
+import dns from 'node:dns'
 import Fastify from 'fastify'
+import container from './infra/container.js'
+import UserRoutes from './interfaces/users/UserRoutes.js'
+
+dns.setServers([ '8.8.8.8', '1.1.1.1' ])
 
 const { PORT } = process.env
 
+const allRoutes = [ ...UserRoutes ]
+
 const fastify = Fastify()
+
+for (const route of allRoutes) {
+  fastify.route(route)
+}
+
+await container.database.connect()
 
 fastify.listen({ port: PORT }, () => console.log(`Server running at http://localhost:${PORT}`))
